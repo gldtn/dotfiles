d1415b379547c3b584ec8a7e148f1889a8278905
gldtn <g@gldtn.io>
2024-05-04 22:01:23 -0400

    highlight colors

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -54,6 +54,27 @@ return {
           hide_by_pattern = { "*-lock.json" },
         },
       },
+      git_status = {
+        window = {
+          position = "float",
+          mappings = {
+            ["A"]  = "git_add_all",
+            ["gu"] = "git_unstage_file",
+            ["ga"] = "git_add_file",
+            ["gr"] = "git_revert_file",
+            ["gc"] = "git_commit",
+            ["gp"] = "git_push",
+            ["gg"] = "git_commit_and_push",
+            ["o"]  = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+            ["oc"] = { "order_by_created", nowait = false },
+            ["od"] = { "order_by_diagnostics", nowait = false },
+            ["om"] = { "order_by_modified", nowait = false },
+            ["on"] = { "order_by_name", nowait = false },
+            ["os"] = { "order_by_size", nowait = false },
+            ["ot"] = { "order_by_type", nowait = false },
+          }
+        }
+      },
       event_handlers = {
         -- Equalize Window Sizes on Neo-tree Open and Close
         {
--- nvim/lua/plugins/tokyo-night.lua
+++ nvim/lua/plugins/tokyo-night.lua
@@ -15,41 +15,47 @@ return {
       ---@param c ColorScheme
       on_highlights = function(hl, c)
         -- define colors
-        local bg, fg, dark, pro_bg, pro_title, pre_title, res_title, highlight
-        bg                         = c.bg
-        fg                         = c.fg
-        dark                       = c.bg_dark
-        pro_bg                     = "#292B41" -- prompt background
-        pro_title                  = c.red     --  prompt title
-        pre_title                  = c.blue    -- preview title
-        res_title                  = c.magenta -- results title
-        highlight                  = c.bg_highlight
-
+        local bg, fg, key, dark, grey, red, blue, green, purple, comment, magenta, highlight
+        bg                           = c.bg
+        fg                           = c.fg
+        dark                         = c.bg_dark
+        grey                         = "#292B41" -- prompt background
+        red                          = c.red     --  prompt title
+        blue                         = c.blue    -- preview title
+        green                        = c.green
+        purple                       = c.purple
+        comment                      = c.comment
+        magenta                      = c.magenta -- results title
+        highlight                    = c.bg_highlight
+        key                          = c.purple
         -- telescope
-        hl.TelescopeNormal         = { bg = dark, fg = fg, }
-        hl.TelescopeBorder         = { bg = dark, fg = dark, }
-        hl.TelescopePromptNormal   = { bg = pro_bg, }
-        hl.TelescopePromptBorder   = { bg = pro_bg, fg = pro_bg, }
-        hl.TelescopePromptTitle    = { bg = pro_title, fg = pro_bg, }
-        hl.TelescopePreviewTitle   = { bg = pre_title, fg = dark, }
-        hl.TelescopeResultsTitle   = { bg = res_title, fg = dark, }
-        hl.NoiceCmdlinePopupBorder = { bg = bg, fg = bg, }
-        hl.TelescopeSelection      = { bg = highlight, fg = fg, }
+        hl.TelescopeNormal           = { bg = dark, fg = fg, }
+        hl.TelescopeBorder           = { bg = dark, fg = dark, }
+        hl.TelescopePromptNormal     = { bg = grey, }
+        hl.TelescopePromptBorder     = { bg = grey, fg = grey, }
+        hl.TelescopePromptTitle      = { bg = red, fg = grey, }
+        hl.TelescopePreviewTitle     = { bg = blue, fg = dark, }
+        hl.TelescopeResultsTitle     = { bg = magenta, fg = dark, }
+        hl.NoiceCmdlinePopupBorder   = { bg = bg, fg = bg, }
+        hl.TelescopeSelection        = { bg = highlight, fg = fg, }
         -- whichkey
-        hl.WhichKeyBorder          = { bg = dark, fg = dark, }
+        hl.WhichKeyBorder            = { bg = dark, fg = dark, }
         -- toggleterm
-        hl.ToggleTermNormal        = { bg = dark, fg = dark, }
-        hl.ToggleTermNormalFloat   = { bg = dark, fg = dark, }
-        hl.ToggleTermFloatBorder   = { bg = dark, fg = dark, }
+        hl.ToggleTermNormal          = { bg = dark, fg = dark, }
+        hl.ToggleTermNormalFloat     = { bg = dark, fg = dark, }
+        hl.ToggleTermFloatBorder     = { bg = dark, fg = dark, }
         -- neogit
-        hl.NeogitNormal            = { bg = bg }
-        hl.NeogitPopup             = { bg = dark }
-        hl.NeogitWinSeparator      = { bg = bg, fg = c.blue }
-        hl.NeogitSectionHeader     = { fg = c.magenta, style = "bold" }
-        hl.NeogitFold              = { fg = c.green }
-        hl.NeogitChangeModified    = { fg = c.blue, style = "bold,italic" }
-        hl.NeogitChangeDeleted     = { fg = c.red, style = "bold,italic" }
-        -- hl.NeogitHunkHeader             = { bg = c.grey, fg = c.bg0, style = "bold" }
+        hl.NeogitNormal              = { bg = bg }
+        hl.NeogitPopupSwitchKey      = { fg = key }
+        hl.NeogitPopupOptionKey      = { fg = key }
+        hl.NeogitPopupActionKey      = { fg = key }
+        hl.NeogitWinSeparator        = { bg = bg, fg = blue }
+        hl.NeogitSectionHeader       = { fg = magenta, style = "bold" }
+        hl.NeogitFold                = { fg = green }
+        hl.NeogitChangeModified      = { fg = blue, style = "bold,italic" }
+        hl.NeogitChangeDeleted       = { fg = red, style = "bold,italic" }
+        hl.NeogitHunkHeader          = { bg = comment, fg = dark, style = "bold" }
+        hl.NeogitHunkHeaderHighlight = { bg = magenta, fg = dark, style = "bold" }
         -- hl.NeogitNotification           = { bg = c.bg0, }
         -- hl.NeogitCursorLine             = { bg = c.bg15, }
         -- hl.NeogitCursorLineNr           = { fg = c.yellow, bg = c.bg15 }
