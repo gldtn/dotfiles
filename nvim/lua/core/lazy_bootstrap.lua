7dba7a7bf59e39d42decf1d5313d6bfae6167c4a
gldtn <g@gldtn.io>
2024-08-09 17:33:33 -0400

    Cyberdream config

--- nvim/lua/core/lazy_bootstrap.lua
+++ nvim/lua/core/lazy_bootstrap.lua
@@ -1,26 +1,37 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  perfomance = {
-    rtp = {
-      disabled_plugins = {
-        "netrwPlugin",
-        "tohtml",
-        "tutor",
-      },
+    ---@diagnostic disable: assign-type-mismatch
+    dev = {
+        path = "~/git-projects",
+        fallback = true,
     },
-  },
-  { import = "plugins" },
-  { import = "plugins.lsp" },
+    ---@diagnostic enable: assign-type-mismatch
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "tarPlugin",
+                "tohtml",
+                "zipPlugin",
+                "netrwPlugin",
+                "tutor",
+            },
+        },
+    },
+    spec = {
+        { import = "plugins" },
+        { import = "plugins.lsp" },
+    },
 })
--- nvim/lua/themes/cyberdream.lua
+++
@@ -1,7 +0,0 @@
-return {
-  require("cyberdream").setup({
-    transparent = true,
-    italic_comments = true,
-  }),
-  vim.cmd("colorscheme cyberdream")
-}
