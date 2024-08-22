5b3c88d52f4a9b9c07a68fd221b5bf694001f5c1
gldtn <g@gldtn.io>
2024-08-22 10:26:40 -0400

    Lazy updates & inactive statuslines

--- nvim/lua/config/feline.lua
+++ nvim/lua/config/feline.lua
@@ -192,10 +192,7 @@ component.vim_mode_icon = {
 	end,
 }
 
--- ------------------------------------
--- git branch
--- ------------------------------------
-
+-- [ git branch ]
 component.git_branch = {
 	provider = "git_branch",
 	icon = {
@@ -219,11 +216,7 @@ component.git_branch = {
 	right_sep = { right_separator },
 }
 
--- ------------------------------------
--- git signs
--- ------------------------------------
-
--- git add
+-- [ git signs ]
 component.git_add = {
 	provider = "git_diff_added",
 	hl = {
@@ -232,7 +225,6 @@ component.git_add = {
 	left_sep = "",
 	right_sep = "",
 }
--- git delete
 component.git_delete = {
 	provider = "git_diff_removed",
 	hl = {
@@ -241,7 +233,6 @@ component.git_delete = {
 	left_sep = "",
 	right_sep = "",
 }
--- git change
 component.git_change = {
 	provider = "git_diff_changed",
 	hl = {
@@ -251,10 +242,7 @@ component.git_change = {
 	right_sep = "",
 }
 
--- ------------------------------------
--- diagnostics
--- ------------------------------------
-
+-- [ diagnostics ]
 component.diagnostic_errors = {
 	provider = "diagnostic_errors",
 	hl = {
@@ -280,9 +268,7 @@ component.diagnostic_info = {
 	},
 }
 
--- ------------------------------------
--- lsp client info
--- ------------------------------------
+-- [ lsp client info ]
 local lsp_icons = {
 	["html"] = "  ",
 	["gopls"] = "  ",
@@ -322,9 +308,7 @@ component.lsp = {
 	right_sep = right_separator,
 }
 
--- ------------------------------------
--- file info
--- ------------------------------------
+-- [ file info ]
 component.file_info = {
 	provider = {
 		name = "file_info",
@@ -335,9 +319,7 @@ component.file_info = {
 	right_sep = right_separator,
 }
 
--- ------------------------------------
--- file type
--- ------------------------------------
+-- [ file type ]
 component.file_type = {
 	provider = {
 		name = "file_type",
@@ -350,9 +332,7 @@ component.file_type = {
 	right_sep = right_separator,
 }
 
--- ------------------------------------
--- position component
--- ------------------------------------
+-- [ position component ]
 component.cursor_position = {
 	provider = function()
 		local line = vim.fn.line(".")
@@ -379,9 +359,7 @@ component.cursor_position = {
 	right_sep = right_separator,
 }
 
--- ------------------------------------
--- scroll bar component
--- ------------------------------------
+-- [ scroll bar component ]
 local function get_scroll_position()
 	local line_ratio = vim.api.nvim_win_get_cursor(0)[1] / vim.api.nvim_buf_line_count(0)
 	return " " .. tostring(math.floor(line_ratio * 100)) .. "%%"
@@ -418,6 +396,63 @@ component.scroll_bar_position = {
 	end,
 	right_sep = right_separator,
 }
+
+local function get_current_time()
+	local time_str = tostring(os.date("%I:%M %p"))
+	return time_str:lower()
+end
+
+component.time_clock = {
+	provider = function()
+		local clock_icon = "  "
+		local time = get_current_time()
+		return clock_icon .. time
+	end,
+	hl = {
+		fg = "fg",
+		bg = "bg",
+		style = "bold",
+	},
+	left_sep = left_separator,
+	right_sep = right_separator,
+}
+
+-- [ Lazy ]
+local has_lazy = function()
+	local lazy_installed, _ = pcall(require, "lazy")
+	return lazy_installed
+end
+
+component.lazy = {
+	provider = function()
+		if has_lazy() then
+			return require("lazy.status").updates()
+		end
+		return ""
+	end,
+	enabled = function()
+		return has_lazy() and require("lazy.status").has_updates()
+	end,
+	hl = function()
+		if has_lazy() and require("lazy.status").has_updates() then
+			return { fg = "green" }
+		else
+			return { fg = "fg" }
+		end
+	end,
+}
+
+-- [ inactive component ]
+component.inactive = {
+	provider = function()
+		return " " .. string.upper(vim.bo.ft) .. " "
+	end,
+	hl = {
+		fg = "fg",
+		bg = "bgAlt",
+	},
+}
+
 vim.api.nvim_set_hl(0, "StatusLine", { bg = theme.bg, fg = theme.fg })
 feline.setup({
 	components = {
@@ -446,15 +481,47 @@ feline.setup({
 			{
 				-- component.file_type,
 				-- component.copilot_status,
+				component.lazy,
+				component.spacer,
 				component.lsp,
 				component.spacer,
+				-- component.time_clock,
+				-- component.spacer,
 				component.cursor_position,
 				component.spacer,
 				component.scroll_bar_icon,
 				component.scroll_bar_position,
 			},
 		},
+		inactive = {
+			{
+				component.inactive,
+			},
+		},
 	},
+	force_inactive = {
+		filetypes = {
+			"^packer$",
+			"^NvimTree$",
+			"^trouble$",
+			"^Outline$",
+			"^qf$",
+			"^help$",
+			"^TelescopePrompt$",
+			"^git$",
+			"^lspinfo$",
+			"^mason$",
+			"^lazy$",
+			"^neo%-tree$",
+			"^Neogit*",
+			"^dashboard$",
+		},
+		buftypes = {
+			"terminal",
+		},
+		bufnames = {},
+	},
+
 	theme = theme,
 	vi_mode_colors = mode_theme,
 })
