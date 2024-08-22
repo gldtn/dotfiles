f26b6a9de54a524702f12ac239e52f5b3144dc80
gldtn <g@gldtn.io>
2024-08-22 10:26:40 -0400

    Lazy updates & inactive statuslines

--- nvim/lua/core/lazy_bootstrap.lua
+++ nvim/lua/core/lazy_bootstrap.lua
@@ -1,37 +1,41 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    ---@diagnostic disable: assign-type-mismatch
-    dev = {
-        path = "~/git-projects",
-        fallback = true,
-    },
-    ---@diagnostic enable: assign-type-mismatch
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                "gzip",
-                "tarPlugin",
-                "tohtml",
-                "zipPlugin",
-                "netrwPlugin",
-                "tutor",
-            },
-        },
-    },
-    spec = {
-        { import = "plugins" },
-        { import = "plugins.lsp" },
-    },
+	---@diagnostic disable: assign-type-mismatch
+	dev = {
+		path = "~/git-projects",
+		fallback = true,
+	},
+	---@diagnostic enable: assign-type-mismatch
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"zipPlugin",
+				"netrwPlugin",
+				"tutor",
+			},
+		},
+	},
+	checker = {
+		enabled = true,
+		frequency = 3600,
+	},
+	spec = {
+		{ import = "plugins" },
+		{ import = "plugins.lsp" },
+	},
 })
