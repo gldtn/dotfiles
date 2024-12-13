e4028a04e01b61e9073cd36cd5115a566c4693c4
gldtn <g@gldtn.io>
2024-12-13 18:32:24 -0500

    Untrack yabai

--- yabai/reload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SID=$(yabai -m query --spaces index --space | jq -r '.index')
-source ~/.config/yabai/yabairc
-yabai -m space --focus $SID
--- yabai/swap-windows.sh
+++
@@ -1,8 +0,0 @@
-win=$(yabai -m query --windows id --window last | jq '.id')
-
-while : ; do
-    yabai -m window $win --swap $1 &> /dev/null
-    if [[ $? -eq 1 ]]; then
-        break
-    fi
-done
